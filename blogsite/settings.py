99ed8f640ca6c00ef21dfef35e22c0c92fe66782
claeryc <reeko.it@gmail.com>
2025-08-18 16:23:56 +0800

    expand the allowed hosts list

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -14,7 +14,9 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
     "claeryc-blog.onrender.com",
     "localhost",
-    "127.0.0.1"
+    "127.0.0.1",
+    "www.claeryc.com",
+    "claeryc.com"
 ]
 
 # Installed apps
