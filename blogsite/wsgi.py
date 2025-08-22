bc8be6b45654c0898d1e3e158fb35f56a4f3b0a4
claeryc <reeko.it@gmail.com>
2025-08-22 19:02:07 +0800

    wsgi change

--- blogsite/wsgi.py
+++ blogsite/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to the Python path
+project_home = '/home/claeryc/myblog/blog-posts'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogsite.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
